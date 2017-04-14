da3e860881ffd9b90a08717c8ad8563ea50e14d3
OrenSchwartz <OrenSchwartz@gmail.com>
2017-04-15 02:01:53 +0300

    now stopping and starting containers

--- server.js
+++ server.js
@@ -27,4 +27,23 @@ io.on('connection', socket => {
     socket.on('containers.list', () => {
         refreshContainers()
     })
+
+    socket.on('container.start', args => {
+        const container = docker.getContainer(args.id)
+
+        if (container) {
+            container.start((err, data) => refreshContainers())
+        }
+    })
+
+    socket.on('container.stop', args => {
+        const container = docker.getContainer(args.id)
+
+        if (container) {
+            container.stop((err, data) => refreshContainers())
+        }
+    })
+
 })
+
+setInterval(refreshContainers, 2000)
